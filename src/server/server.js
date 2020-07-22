a02b0249aa617cbab857bdb857128f01a5989b30
intellerX <anmevel10@gmail.com>
2020-07-21 21:00:20 -0500

    Deploy Video app

--- src/server/server.js
+++ src/server/server.js
@@ -1,23 +1,21 @@
 /* eslint-disable func-names */
-/* eslint-disable global-require */
 import express from 'express';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
-import webpack from 'webpack';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
-import { renderRoutes } from 'react-router-config';
+import { renderRoutes } from 'react-router-config'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { StaticRouter } from 'react-router-dom';
 import cookieParser from 'cookie-parser';
 import boom from '@hapi/boom';
 import passport from 'passport';
 import axios from 'axios';
 import reducer from '../frontend/reducers';
-import Layout from '../frontend/components/Layout';
-import serverRoutes from '../frontend/routes/serverRoutes';
 import getManifest from './getManifest';
+import serverRoutes from '../frontend/routes/serverRoutes';
+import Layout from '../frontend/components/Layout';
 
 
 dotenv.config();
@@ -32,79 +30,73 @@ app.use(passport.session());
 
 require('./utils/auth/strategies/basic');
 
-if (ENV === 'development') {
-  const webPackConfig = require('../../webpack.config');
-  const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
-  const compiler = webpack(webPackConfig);
-  const serverConfig = { port: PORT, hot: true };
-  app.use(webpackDevMiddleware(compiler, serverConfig));
-  app.use(webpackHotMiddleware(compiler));
-} else {
-  app.use((req, res, next) => {
-    req.hashManifest = getManifest();
+
+  app.use( (req, res, next) => {
+    if (!req.hashManifest) req.hashManifest = getManifest();
     next();
   });
+  app.use(express.static(`${__dirname}/public`));
   app.use(helmet());
   app.use(helmet.permittedCrossDomainPolicies());
   app.disable('x-powered-by');
-}
+
 
 const setResponse = (html, preloadedState, manifest) => {
-  const mainStyles = manifest ? manifest['main.css'] : '/assets/app.css';
-  const mainBuild = manifest ? manifest['main.js'] : '/assets/app.js';
+  const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
+  const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
   const vendorBuild = manifest ? manifest['vendors.js'] : 'assets/vendor.js';
-  return (
-    `
+
+  return (`
       <!DOCTYPE html>
-      <html lang="es">
-        <head>
+      <html lang="en">
+      <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-          <meta charset="utf-8" />          
-          <link rel="stylesheet" href="${mainStyles}" type="text/html"/>
-          <title>Platfix</title>
-        </head>
-        <body>
+          <link href=${mainStyles} rel="stylesheet" type="text/css"></link>
+          <title>Platzi Video | App</title>
+      </head>
+      <body>
           <div id="app">${html}</div>
-          <script id="preloadedState">
-            window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+          <script>
+            window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g,'\\u003c')}
           </script>
-          <script src="${mainBuild}" type="text/javascript"></script>
-          <script src="${vendorBuild}" type="text/javascript"></script>
-        </body>
-      </html>`
-  );
-};
+          <script src=${mainBuild} type="text/javascript"></script>
+          <script src=${vendorBuild} type="text/javascript"></script>
+      </body>
+      </html>
+  `);
+}
 
 const renderApp = async (req, res) => {
+
   let initialState;
-  const { token, email, name, id } = req.cookies;
+  const { token ,email, name, id } = req.cookies;
 
   try {
     let movieList = await axios({
       url: `${process.env.API_URL}/api/movies`,
-      headers: { Authorization: `Bearer ${token}`},
-      method: 'get',
+      headers: { Authorization: `Bearer ${token}` },
+      method: 'get'
     });
+
     movieList = movieList.data.data;
+
     initialState = {
       user: {
-        id, email, name,
+        id, email, name
       },
       myList: [],
-      trends: movieList.filter(movie => movie.contentRating === 'PG' &&movie._id),
-      originals: movieList.filter(movie => movie.contentRating === 'G'&& movie._id)
+      trends: movieList.filter(movie => movie.contentRating === 'PG' && movie._id),
+      originals: movieList.filter(movie => movie.contentRating === 'G' && movie._id)
     };
+
   } catch (err) {
     initialState = {
       user: {},
-      myList: [],
       trends: [],
       originals: []
-    }
-  }
+    };
+  };
 
   const store = createStore(reducer, initialState);
   const preloadedState = store.getState();
@@ -117,64 +109,74 @@ const renderApp = async (req, res) => {
         </Layout>
       </StaticRouter>
     </Provider>
-  )
+  );
+
   res.send(setResponse(html, preloadedState, req.hashManifest));
 };
 
-app.post("/auth/sign-in", async function (req, res, next) {
-  passport.authenticate("basic", function (error, data) {
+app.post('/auth/sign-in', async function(req, res, next) {
+
+  // get attr from body request
+  // const { rememberMe } = req.body;
+
+  passport.authenticate('basic', function(error, data) {
     try {
       if (error || !data) {
         next(boom.unauthorized());
-      }
+      };
 
-      req.login(data, { session: false }, async function (err) {
+      req.login(data, { session: false }, async function(err) {
         if (err) {
           next(err);
-        }
+        };
 
         const { token, ...user } = data;
 
-        res.cookie("token", token, {
+        res.cookie('token', token, {
           httpOnly: !(ENV === 'development'),
           secure: !(ENV === 'development')
+          // maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC,
         });
 
         res.status(200).json(user);
+
       });
-    } catch (err) {
+    }catch(err) {
       next(err);
-    }
+    };
+
   })(req, res, next);
 });
 
-app.post("/auth/sign-up", async function (req, res, next) {
+app.post("/auth/sign-up", async function(req, res, next) {
   const { body: user } = req;
 
   try {
     const userData = await axios({
       url: `${process.env.API_URL}/api/auth/sign-up`,
-      method: "post",
+      method: 'post',
       data: {
         'email': user.email,
         'name': user.name,
         'password': user.password
-      }
+      },
     });
+
     res.status(201).json({
       name: req.body.name,
       email: req.body.email,
       id: userData.data.id
     });
-  } catch (error) {
-    next(error);
-  }
-});
 
+  }catch(error) {
+    next(error);
+  };
+
+});
 
 app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
-  else console.log(`${ENV} server running on Port ${PORT}`);
+  else console.log(`The server is running on PORT ${PORT}`);
 });
